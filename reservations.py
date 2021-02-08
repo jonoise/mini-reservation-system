021b08001c0c3d77a937e54773fec1dd031f9868
YellowBallPython <yellowballpython@gmail.com>
2021-02-08 02:20:45 -0600

    Comments added :)

--- reservations.py
+++ reservations.py
@@ -3,16 +3,21 @@ import datetime as dt
 
 class Reserva():
 
-    def __init__(self, fecha, entrada, salida):
+    def __init__(self, entrada, salida, fecha,):
         self.entrada = dt.time(hour=entrada, minute=0)
         self.salida = dt.time(hour=salida, minute=0)
         self.fecha = fecha
+
+    """Agregué este método, lo pueden usar con cada reserva
+    por ejemplo: print(mi_reserva.info())"""
 
     def info(self):
         return f'Fecha: {self.fecha}, check in: {self.entrada} - check out: {self.salida}'
 
 
 def desplegar_r(all_res):
+    """ También hice esta función para desplegar la lista de reservaciones
+    y poder visualizar si en efecto, nuestra reserva fue aceptada o rechazada."""
     for r in all_r:
         print(r.info())
 
@@ -30,9 +35,12 @@ all_r = [r1, r2, r3, r4]
 mi_reserva = Reserva(12, 17, fecha)
 
 for i in range(1, len(all_r)):
+    # Asignamos la reserva siguiente y la anterior,
+    # colocamos la nuestra "en el medio" por así decirlo.
     r_siguiente = all_r[i]
     r_anterior = all_r[i-1]
 
+    # Hace la comparación de horas.
     if mi_reserva.entrada >= r_anterior.salida and mi_reserva.salida <= r_siguiente.entrada:
         all_r.insert(i, mi_reserva)
         break
